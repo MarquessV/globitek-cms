12730e80cd87e4ff1bb35ccb675d9e890d27390e
Marquess Valdez <marquessavaldez@gmail.com>
2017-02-03 16:14:08 -0800

    Completed basic form and submission to database

--- public/register.php
+++ public/register.php
@@ -1,36 +1,55 @@
 <?php
   require_once('../private/initialize.php');
+  include '../private/validate_functions.php';
+  //include '../private/functions.php';
 
   // Set default values for all variables the page needs.
+  
+  $username=$firstname=$lastname=$email="";
+  $firsterror=$lasterror=$emailerror=$usererror=False;
 
   // if this is a POST request, process the form
   // Hint: private/functions.php can help
-
-    // Confirm that POST values are present before accessing them.
-
-    // Perform Validations
-    // Hint: Write these in private/validation_functions.php
-
-    // if there were no errors, submit data to database
-
-      // Write SQL INSERT statement
-      // $sql = "";
-
-      // For INSERT statments, $result is just true/false
-      // $result = db_query($db, $sql);
-      // if($result) {
-      //   db_close($db);
-
-      //   TODO redirect user to success page
-
-      // } else {
-      //   // The SQL INSERT statement failed.
-      //   // Just show the error, not the form
-      //   echo db_error($db);
-      //   db_close($db);
-      //   exit;
-      // }
-
+  if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if(isset($_POST['firstname'])) {
+        $firstname=$_POST['firstname'];
+        if(!has_length($firstname, array('min' => 1, 'max' => 25))) {
+            $firsterror=True; 
+        }
+    }
+    if(isset($_POST['lastname'])) {
+        $lastname=$_POST['lastname'];
+        if(!has_length($lastname, array('min' => 1, 'max' => 25))) {
+            $lasterror=True; 
+        }
+    }
+    if(isset($_POST['email'])) {
+        $email=$_POST['email'];
+        if(!has_valid_email_format($email)) {
+            $emailerror=True; 
+        }
+    }
+    if(isset($_POST['username'])) {
+        $username=$_POST['username'];
+        if(!has_length($username, array('min' => 1, 'max' => 25))) {
+            $usererror=True; 
+        }
+    }
+    if(!$firsterror && !$lasterror && !$emailerror && !$usererror) {
+        $sql = "INSERT INTO users (first_name, last_name, email, username)
+                VALUES (\"$firstname\", \"$lastname\", \"$email\", \"$username\")";
+        $result = db_query($db, $sql);
+        if($result) {
+            db_close($db);
+            // TODO redirect to success page
+        }
+        else {
+            echo db_error($db);
+            db_close($db);
+            exit;
+        }
+    }
+  }
 ?>
 
 <?php $page_title = 'Register'; ?>
@@ -41,11 +60,33 @@
   <p>Register to become a Globitek Partner.</p>
 
   <?php
-    // TODO: display any form errors here
-    // Hint: private/functions.php can help
+    if($firsterror) {
+        echo "Invalid firstname <br>";
+    }
+    if($lasterror) {
+        echo "Invalid lastname <br>";
+    }
+    if($emailerror) {
+        echo "Invalid email <br>";
+    }
+    if($usererror) {
+        echo "Invalid username <br>";
+    }
   ?>
 
-  <!-- TODO: HTML form goes here -->
+  <form action="" method="post">
+    First name: <br>
+    <input type="text" name="firstname" value="<?php echo $firstname ?>"> <br>
+    Last name: <br>
+    <input type="text" name="lastname" value="<?php echo $lastname ?>"> <br>
+    Email: <br>
+    <input type="text" name="email" value="<?php echo $email ?>"> <br>
+    Username: <br>
+    <input type="text" name="username" value="<?php echo $username ?>"> <br>
+    <br>
+    <input type="submit" value="Submit"> <br>
+  </form>
+    
 
 </div>
 
